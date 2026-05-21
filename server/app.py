51f8668978cec0fd18014b1c9ed8ad4c85989db3
Katherine <kaburbank@gmail.com>
2026-05-20 18:09:39 -0600

    initialize database and migrate

--- server/app.py
+++ server/app.py
@@ -1,16 +1,21 @@
 from flask import Flask, make_response
 from flask_migrate import Migrate
-from models import *
+from .models import db, Exercise, Workout, WorkoutExercise
+import os
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///app.db')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-migrate = Migrate(app, db)
+    db.init_app(app)
+    Migrate(app, db)
 
-db.init_app(app)
+    # Define Routes here
 
-# Define Routes here
+    return app
+
+app = create_app()
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
